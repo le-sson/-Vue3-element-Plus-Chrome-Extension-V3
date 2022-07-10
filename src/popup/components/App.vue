48e52e36b33d9a6c0b2387d25c6f46c0354d87f6
林轩 <hexuanlin@58.com>
2022-07-10 22:26:44 +0800

    add

---
+++ src/popup/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="popup-header">hello Popup</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.popup-header {
+	width: 200px;
+	height: 100px;
+	color: #fff;
+	line-height: 100px;
+	text-align: center;
+	font-size: 20px;
+	background: #369;
+}
+</style>
