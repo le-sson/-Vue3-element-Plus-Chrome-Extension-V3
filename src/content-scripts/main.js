3b26de67fae1e07af876858421e3129b71e14a0d
lisheng <lisheng@zingfront.com>
2023-09-25 11:51:39 +0800

    feat: 自定义路由匹配规则

--- src/content-scripts/main.js
+++ src/content-scripts/main.js
@@ -1,15 +1,22 @@
 import { createApp } from 'vue'
 import App from '@/content-scripts/App.vue';
-// import store from '@/content-scripts/store'; // VueX 酌情考虑是否使用
+import store from '@/content-scripts/store'; // VueX 酌情考虑是否使用
 import ElementPlus from 'element-plus'; // vue3 中需使用 ElementPlus
+import { matchPattern } from '@/utils/func.js';
 
 
 document.addEventListener('DOMContentLoaded', function () {
-	const div = document.createElement('div');
-    div.id = 'content-div';
-    document.body.appendChild(div);
-    createApp(App)
-        // .use(store)
-        .use(ElementPlus)
-        .mount('#content-div'); 
+    let url = window.location.href;
+    if (matchPattern('', url)) {
+        const div = document.createElement('div');
+        div.id = 'content-div';
+        document.body.appendChild(div);
+        createApp(App)
+            .use(store)
+            .use(ElementPlus)
+            .mount('#content-div'); 
+    }
 });
+
+
+
